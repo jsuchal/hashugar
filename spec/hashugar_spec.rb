c6a46c2b76eb1a09a3fcb08f8c1ba48e0cafe280
Kenn Ejima <kenn.ejima@gmail.com>
2014-11-19 20:10:14 -0800

    RSpec 3

--- spec/hashugar_spec.rb
+++ spec/hashugar_spec.rb
@@ -4,73 +4,73 @@ describe Hashugar do
   context 'when accessing simple hash' do
     it 'should be make accessible string and symbol keys' do
       hashugar = {:a => 1, 'b' => 2}.to_hashugar
-      hashugar.a.should == 1
-      hashugar.b.should == 2
+      expect(hashugar.a).to eq(1)
+      expect(hashugar.b).to eq(2)
     end
 
     it 'should be readable through nice methods' do
       hashugar = {:a => 1, :b => 2}.to_hashugar
-      hashugar.a.should == 1
-      hashugar.b.should == 2
+      expect(hashugar.a).to eq(1)
+      expect(hashugar.b).to eq(2)
     end
 
     it 'should be writable through nice methods' do
       hashugar = {:a => 1}.to_hashugar
       hashugar.a = 2
       hashugar.b = 3
-      hashugar.a.should == 2
-      hashugar.b.should == 3
+      expect(hashugar.a).to eq(2)
+      expect(hashugar.b).to eq(3)
     end
 
     it 'should be readable through old methods' do
       hashugar = {:a => 1}.to_hashugar
-      hashugar[:a].should == 1
-      hashugar['a'].should == 1
+      expect(hashugar[:a]).to eq(1)
+      expect(hashugar['a']).to eq(1)
     end
 
     it 'should be writable through old methods' do
       hashugar = {:a => 1}.to_hashugar
       hashugar['a'] = 2
-      hashugar.a.should == 2
+      expect(hashugar.a).to eq(2)
       hashugar[:a] = 3
-      hashugar.a.should == 3
+      expect(hashugar.a).to eq(3)
     end
   end
 
   context 'when accessing nested hash' do
     it 'should be writable through nice methods' do
       hashugar = {:a => {:b => 1}}.to_hashugar
-      hashugar.a.b.should == 1
+      expect(hashugar.a.b).to eq(1)
     end
 
     it 'should be writable through nice methods' do
       hashugar = {:a =>{}}.to_hashugar
       hashugar.a.b = 1
-      hashugar.a.b.should == 1
+      expect(hashugar.a.b).to eq(1)
     end
   end
 
   context 'when accessing hashes in array' do
     it 'should return hashugars' do
       hashugar = [{:a => 1}, {:b => 2}].to_hashugar
-      hashugar[0].a.should == 1
-      hashugar.last.b.should == 2
+      expect(hashugar[0].a).to eq(1)
+      expect(hashugar.last.b).to eq(2)
     end
   end
 
   context 'when using respond_to?' do
     it 'should return true on valid key' do
       hashugar = {:a => 1}.to_hashugar
-      hashugar.respond_to?('a').should be_true
-      hashugar.respond_to?(:a).should be_true
-      hashugar.respond_to?(:b).should be_false
+      expect(hashugar.respond_to?('a')).to be_truthy
+      expect(hashugar.respond_to?(:a)).to be_truthy
+      expect(hashugar.respond_to?(:b)).to be_falsey
     end
   end
 
   context 'when creating using Hashugar#new' do
     it 'should accept hash in contructor' do
       hashugar = Hashugar.new({:a => {:b => 1}})
-      hashugar.a.b.should == 1
+      expect(hashugar.a.b).to eq(1)
     end
   end
 
@@ -85,8 +85,8 @@ describe Hashugar do
         values << v
       end
 
-      keys.should == [:a, :c]
-      values.should == [4, 2]
+      expect(keys).to eq([:a, :c])
+      expect(values).to eq([4, 2])
     end
   end
 end
